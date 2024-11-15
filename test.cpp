b03d3ba059b122f9b8dbb67f97939574d46e0c88
Jok3Noname <63104855+JackDonna@users.noreply.github.com>
2024-11-15 18:21:11 +0100

    first commit

---
+++ test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zachf on 15/11/2024.
+//
+
+#include <iostream>
+#include <SFML/System/String.hpp>
+
+std::string test() {
+    return "test";
+}
---
+++ test.h
@@ -0,0 +1,12 @@
+//
+// Created by zachf on 15/11/2024.
+//
+
+#ifndef TEST_H
+#define TEST_H
+#include <SFML/System/String.hpp>
+#include <iostream>
+
+std::string test();
+
+#endif //TEST_H
